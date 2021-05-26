a9975d613e3401ce1edd90292bff8c9c3c51b8f8
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-05-26 16:48:11 +0200

    feat: update backtracking solution for 8 queens puzzles

--- learning/01-pattern/00-backtracking/00-eight-queens-bt.py
+++ learning/01-pattern/00-backtracking/00-eight-queens-bt.py
@@ -4,17 +4,42 @@ board = np.full((8, 8), 0)
 
 
 def print_board(bo):
-    for r in bo:
-        for c in r:
+    for d in bo:
+        for c in d:
             print(c, end=" ")
         print()
 
 
-def possible(x, y):
-    for i in range(x):
-        if a[i] == y or x + y == i + a[i] or x - y == i - a[i]:
+def solve(bo):
+    l = len(bo)
+    for d in range(l):
+        for c in range(l):
+            if bo[d][c] == 0:
+                if possible(bo, d, c):
+                    bo[d][c] = 1
+                    solve(bo)
+                if sum(sum(a) for a in bo) == l:
+                    return bo
+                else:
+                    bo[d][c] = 0
+    return bo
+
+
+def possible(bo, d, c):
+    l = len(bo)
+    for i in range(l):
+        if bo[i][c] == 1:
             return False
+    for i in range(l):
+        if bo[d][i] == 1:
+            return False
+    for i in range(l):
+        for j in range(l):
+            if bo[i][j] == 1:
+                if c - d == j - i or c + d == i + j:
+                    return False
     return True
 
 
+solve(board)
 print_board(board)
